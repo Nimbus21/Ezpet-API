230db929e6b99c2b5710f70e95950a4eb617909b
Nimbus21 <thandy_97@hotmail.com>
2021-02-01 20:10:41 -0300

    cookie route

--- src/main/java/br/com/ezpet/nimbus21/config/WebSecurityConfig.java
+++ src/main/java/br/com/ezpet/nimbus21/config/WebSecurityConfig.java
@@ -1,16 +1,25 @@
 package br.com.ezpet.nimbus21.config;
 
+import javax.servlet.Filter;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CsrfFilter;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-//	@Override
-//	protected void configure(HttpSecurity http) throws Exception {
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
 //		http.csrf().disable();
-//	}
+		http.addFilterAfter(csrfCookieFilter(), CsrfFilter.class);
+	}
+	
+	// javax.servlet.Filter;
+	protected Filter csrfCookieFilter() {
+		return new CsrfCookieFilter();
+	}
 }
--- src/main/java/br/com/ezpet/nimbus21/resource/MainResource.java
+++ src/main/java/br/com/ezpet/nimbus21/resource/MainResource.java
@@ -1,8 +1,16 @@
 package br.com.ezpet.nimbus21.resource;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MainResource {
 
+	@GetMapping("/cookie")
+	@ResponseStatus(code = HttpStatus.OK)
+	public String cookie() {
+		return "cookie";
+	}
 }
