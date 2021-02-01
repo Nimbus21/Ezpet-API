3cbe6f101d8c61a2299bf6519571fa87e028a076
Nimbus21 <thandy_97@hotmail.com>
2021-02-01 20:34:26 -0300

    removed filter from config

--- src/main/java/br/com/ezpet/nimbus21/config/WebSecurityConfig.java
+++ src/main/java/br/com/ezpet/nimbus21/config/WebSecurityConfig.java
@@ -15,7 +15,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 //		http.csrf().disable();
-		http.addFilterAfter(csrfCookieFilter(), CsrfFilter.class);
+//		http.addFilterAfter(csrfCookieFilter(), CsrfFilter.class);
 	}
 	
 	// javax.servlet.Filter;
