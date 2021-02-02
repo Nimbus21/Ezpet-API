5d8962e6bd0c4f022776d43d5bf3ca379eea6107
Nimbus21 <thandy_97@hotmail.com>
2021-02-02 08:16:27 -0300

    cors mapping

---
+++ src/main/java/br/com/ezpet/nimbus21/config/WebConfig.java
@@ -0,0 +1,12 @@
+package br.com.ezpet.nimbus21.config;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfig implements WebMvcConfigurer{
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**");
+	}
+}
