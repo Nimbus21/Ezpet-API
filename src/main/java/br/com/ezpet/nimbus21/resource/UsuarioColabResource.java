e70900389a60df609f367954a6931a85052a2189
Nimbus21 <thandy_97@hotmail.com>
2021-02-04 01:07:17 -0300

    findByEmail route added

--- src/main/java/br/com/ezpet/nimbus21/resource/UsuarioColabResource.java
+++ src/main/java/br/com/ezpet/nimbus21/resource/UsuarioColabResource.java
@@ -1,10 +1,13 @@
 package br.com.ezpet.nimbus21.resource;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,5 +29,12 @@ public class UsuarioColabResource {
 		List<UsuarioColaborador> usuariosColab = usuarioColabRepo.findAll();
 		
 		return UsuarioColaboradorDTO.converter(usuariosColab);
+	}
+	
+	@GetMapping("/email/{email}")
+	public ResponseEntity<UsuarioColaboradorDTO> readEmail(@PathVariable("email") String email) {
+		Optional<UsuarioColaborador> usuarioColab = usuarioColabRepo.findByEmail(email);
+		
+		return usuarioColab.map(u -> ResponseEntity.ok(new UsuarioColaboradorDTO(u))).orElse(ResponseEntity.notFound().build());
 	}
 }
