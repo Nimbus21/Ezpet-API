ffee648c37237d005fadeb4f162dfc0684e34b96
Nimbus21 <thandy_97@hotmail.com>
2021-03-01 22:36:35 -0300

    deletemapping adicionado ao usuariocomercialresource

--- src/main/java/br/com/ezpet/nimbus21/resource/UsuarioComercialResource.java
+++ src/main/java/br/com/ezpet/nimbus21/resource/UsuarioComercialResource.java
@@ -8,10 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -147,6 +147,16 @@ public class UsuarioComercialResource {
 			u.setRole(usuarioComercialNovo.getRole());
 			usuarioComercialRepo.save(u);
 			return ResponseEntity.ok(new UsuarioComercialDTO(u));
+		}).orElse(ResponseEntity.notFound().build());
+	}
+	
+	@DeleteMapping("{id}")
+	@Transactional
+	public ResponseEntity<?> delete(@PathVariable("id") Long codigo) {
+		Optional<UsuarioComercial> usuarioComercial = usuarioComercialRepo.findById(codigo);
+		return usuarioComercial.map(u -> {
+			usuarioComercialRepo.deleteById(codigo);
+			return ResponseEntity.ok().build();
 		}).orElse(ResponseEntity.notFound().build());
 	}
 	
